781d92bee751020431205df0b38ecb809d7134b7
fcl <322d@qq.com>
2025-12-09 09:44:54 +0800

    update

---
+++ src/api/order/index.js
@@ -0,0 +1,45 @@
+import request from '@/utils/request'
+
+export default {
+  checkCart(dto) {
+    return request({
+      url: '/order/check/cart',
+      method: 'post',
+      data: dto,
+    })
+  },
+  checkDirect(dto) {
+    return request({
+      url: '/order/check/direct',
+      method: 'post',
+      data: dto,
+    })
+  },
+  create(dto) {
+    return request({
+      url: '/order/create',
+      method: 'post',
+      data: dto,
+    })
+  },
+  query(dto) {
+    return request({
+      url: '/order/query',
+      method: 'post',
+      data: dto,
+    })
+  },
+  pay(id) {
+    return request({
+      url: '/order/pay/' + id,
+      method: 'post',
+    })
+  },
+  cancel(dto) {
+    return request({
+      url: '/order/cancel',
+      method: 'post',
+      data: dto,
+    })
+  },
+}
